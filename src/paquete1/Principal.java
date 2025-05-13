2faa0434342902d4031e74ccd1957c4f7563c9b6
Pablo <Pablo@HPLaptop14>
2025-05-13 00:15:18 -0600

    tercer commit de prueba ahora con un add

--- src/paquete1/Principal.java
+++ src/paquete1/Principal.java
@@ -3,6 +3,5 @@ package paquete1;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Proyecto Final - Programacion 3");
-        System.out.println("Hola");
     }
 }
