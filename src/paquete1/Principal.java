24d674fbe8642afabd58db794da135cc57c6e39b
Pablo <Pablo@HPLaptop14>
2025-05-13 00:00:13 -0600

    segundo commit de prueba con el proyecto clonado en NetBeans

--- src/paquete1/Principal.java
+++ src/paquete1/Principal.java
@@ -3,5 +3,6 @@ package paquete1;
 public class Principal {
     public static void main(String[] args) {
         System.out.println("Proyecto Final - Programacion 3");
+        System.out.println("Hola");
     }
 }
